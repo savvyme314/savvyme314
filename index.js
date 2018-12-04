f4cc666f48d39b3c500db7879ee5093bf5f917b6
sonni edwards <son.e@cheerful.com>
2018-12-03 18:29:44 -0600

    belated commits including demosite idea

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-var username = prompt('What is your Name?');
-var greet = function greet() {
-
-    if (username === '') {
-        username = prompt('what is your name foreal this time?')
-
-        greet();
-    } else {
-        alert('hello' + username);
-    }
-}
+import Content from './src/Content';
+import Footer from './src/Footer';
+import greet from './src/Greeting';
+import Header from './src/Header';
+import Navigation from './src/Navigation';
+var state = {
+    'title': 'Welcome to my Savvy Coders Portfolio Project'
+};
+document.querySelector('#root').innerHTML = `
+   ${Navigation}
+   ${Header}
+   ${Content}
+   ${Footer}
+   ${Navigation(state)}
+   ${Header(state)}
+   ${Content(state)}
+   ${Footer(state)}
+`;
+greet();
---
+++ src/Content.js
@@ -0,0 +1,16 @@
+export default `
+  <div id="content">
+    <div class="container">
+      <p>I teach HTML, CSS, and JavaScript with Savvy Coders. I think web development is the bee's knees.</p>
+    </div>
+  </div>
+`;
+export default function Content() {
+  return `
+      <div id="content">
+        <div class="container">
+          <p>I teach HTML, CSS, and JavaScript with Savvy Coders. I think web development is the bee's knees.</p>
+        </div>
+      </div>
+    `;
+}
